baf5e949b2858dd3098f22728e6e30e337ae4d68
joni <jonatan@jacoti.com>
2024-02-27 12:55:13 +0100

    moved import automations to main execution module dependency in application template

--- ha_control/templates/application.py
+++ ha_control/templates/application.py
@@ -1,8 +1,8 @@
-import automations
 import ha_control
 
 
 if __name__ == '__main__':
+    import automations
     ha_url, ha_token = ha_control.get_secrets()
     registry = ha_control.get_registry()
     app = ha_control.Application(automations, ha_url, ha_token, registry)
